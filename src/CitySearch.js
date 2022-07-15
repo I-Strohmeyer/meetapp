f2e2fbabad774d76d215d636afeefd9ea867c94c
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2022-07-15 17:00:14 +0200

    add info & error alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,27 +1,41 @@
 import React, { Component } from "react";
 
+import { InfoAlert } from "./Alert";
+
 export class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
     showSuggestions: false,
+    infoText: "",
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
       showSuggestions: false,
+      infoText: "",
     });
 
     this.props.updateEvents(suggestion, undefined);
@@ -30,6 +44,7 @@ export class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           placeholder="Search events by city"
           type="text"
@@ -39,6 +54,7 @@ export class CitySearch extends Component {
           onFocus={() => {
             this.setState({ showSuggestions: true });
           }}
+          /*onBlur etc fix suggestions    */
         />
         <ul
           className="suggestions"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,37 +1,52 @@
 import React, { Component } from "react";
 
+import { ErrorAlert } from "./Alert";
+
 export class NumberOfEvents extends Component {
   state = {
     eventsNumber: 32,
+    errorText: "",
   };
 
   handleNumOfEvents = (e) => {
     //let updatedNumber = parseInt(e.target.value);
     let updatedNumber = e.target.value;
-    this.setState({
-      eventsNumber: updatedNumber,
-    });
-    this.props.updateNumberEvents(updatedNumber);
+    if (updatedNumber > 32 || updatedNumber < 1) {
+      this.setState({
+        eventsNumber: updatedNumber,
+        errorText: "Please select a number between 1 and 32.",
+      });
+    } else {
+      this.setState({
+        eventsNumber: updatedNumber,
+        errorText: "",
+      });
+    }
+    this.props.updateNumberEvents(undefined, updatedNumber);
+    console.log(this.errorText);
   };
 
   render() {
     return (
-      <div className="NumberOfEvents">
-        <div className="results">Results</div>
-        <div className="labelinput">
-          <label htmlFor="number">Show</label>
-          <input
-            className="eventsnumber_input"
-            name="number"
-            id="number"
-            type="number"
-            min="1"
-            max="32"
-            value={this.state.eventsNumber}
-            onChange={this.handleNumOfEvents}
-          ></input>
+      <>
+        <div className="NumberOfEvents">
+          <div className="results">Results</div>
+          <div className="labelinput">
+            <label htmlFor="number">Show</label>
+            <input
+              className="eventsnumber_input"
+              name="number"
+              id="number"
+              type="number"
+              min="1"
+              max="32"
+              value={this.state.eventsNumber}
+              onChange={this.handleNumOfEvents}
+            ></input>
+          </div>
         </div>
-      </div>
+        <ErrorAlert text={this.state.errorText} />
+      </>
     );
   }
 }
